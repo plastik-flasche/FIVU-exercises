749392ca5d39b88c5046699898a30b75b42069e0
plastik-flasche <plastikflasche@duck.com>
2023-11-22 09:50:36 +0100

    completed ue02

--- ue02_uart_exercise/main.c
+++ ue02_uart_exercise/main.c
@@ -1,83 +1,63 @@
+#include <avr/io.h>
 #include <stdio.h>
 #include <string.h>
-
-#include <avr/io.h>
 #include <util/delay.h>
 
-int main ()
-{
-    //TODO: configure the onboard LED PB5 as output and clear the LED
-    DDRB = ?;
-    PORTB &= ?;
+int main() {
+    DDRB = (1 << PB5);
+    PORTB &= ~(1 << PB5);
 
-    //Initialize the UART with the settings: 57600/8N1
+    // Initialize the UART with the settings: 57600/8N1
 
-    //TODO: Setup up the baudrate with 57600 bps -> configure the registers UBRR0H, UBRR0L and UCSR0A -> page 199-200
-	//Note: Register UBRR0 is a 16-bit register. 
-    UBRR0H = ?;
-    UBRR0L = ?;
-	UCSR0A = ?;
+    // Note: Register UBRR0 is a 16-bit register.
+    UBRR0H = 0;
+    UBRR0L = 34;
+    UCSR0A = (1 << 1);
 
-    //TODO: configure the asynchronous communcation with the data frame format 8N1 -> page 202, 201
-	//UCSR0C = 0;
-	//TODO enable the receiver and transmitter peripheral 
-    UCSR0B = ?;
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 
     char text[100] = "Hello World!\n";
-    for (uint8_t i=0; i < sizeof(text); i++)
-    {
-        //Wait until the transmit buffer is ready to receive new data -> see register UCSR0A on page 200
+    for (uint8_t i = 0; i < sizeof(text); i++) {
+        // Wait until the transmit buffer is ready to receive new data -> see register UCSR0A on page 200
         //
-        while (!(UCSR0A & (1 << UDRE0))) 
+        while (!(UCSR0A & (1 << UDRE0)))
             ;
-        //TODO: transmit the character
-        UDR0 = ?;
+        UDR0 = text[i];
     }
 
-    while (1)
-    {
-        // TODO:    add a condition to check if a character was received by the UART peripheral.
-        //          E.g. the user pressed a key on the keyboard. 
-        //          -> see corresponding bit in register UCSR0A -> page 200
-        if (UCSR0A & ?) 
-        {
+    while (1) {
+        if (UCSR0A & (1 << RXC0)) {
             char receivedKey = UDR0;
-            switch (receivedKey)
-            {
-                case '1':   //turn on the onboard led on port PB5
+            switch (receivedKey) {
+                case '1':  // turn on the onboard led on port PB5
                 {
-                    //The following text will be sent to the computer in a later step. 
+                    // The following text will be sent to the computer in a later step.
                     strcpy(text, "LED switched on!\n");
-                    //TODO: turn on the obnard led on port PB5
-                    PORTB |= ?;
+                    PORTB |= (1 << PB5);
                     break;
                 }
-                
-                case '0':   //turn off the onboard led on port PB5
-                {
-                    //The following text will be sent to the computer in a later step. 
-                    strcpy(text, "led switched off!\n");
-                    //TODO: turn ff the obnard led on port PB5
-                    PORTB &= ?;
-                    break;
-                }                            
 
-                default:
+                case '0':  // turn off the onboard led on port PB5
                 {
-                    //The following text will be sent to the computer in a later step. 
+                    // The following text will be sent to the computer in a later step.
+                    strcpy(text, "LED switched off!\n");
+                    PORTB &= ~(1 << PB5);
+                    break;
+                }
+
+                default: {
+                    // The following text will be sent to the computer in a later step.
                     strcpy(text, "unknown command!\n");
                 }
-
             }
 
-            //A response is sent to the computer. 
-            for (uint8_t i = 0; text[i] != 0; i++)
-            {
-                //Wait until the transmit buffer is ready to receive new data -> see register UCSR0A on page 200
-                while (!(UCSR0A & (1 << UDRE0))) 
+            // A response is sent to the computer.
+            for (uint8_t i = 0; text[i] != 0; i++) {
+                // Wait until the transmit buffer is ready to receive new data -> see register UCSR0A on page 200
+                while (!(UCSR0A & (1 << UDRE0)))
                     ;
-                //TODO: transmit the character
-                UDR0 = ?;
+                UDR0 = text[i];
             }
             text[0] = '\0';
         }
